a82f7a7bdb8d995fea1569ad38d962c91079fb69
jenelynababan <jenelynababan@priume.com>
2016-07-20 18:46:04 +0800

    login controller
    login logic

--- controllers/user.js
+++ controllers/user.js
@@ -3,63 +3,65 @@ function User() {
 	tomodel   = {};
 	model 	  = require('../models/User_model');
     passport  = require('../config/auth');
-
 };
  
 User.prototype.constructor = User;
 
-User.prototype.viewLogin =  function(req, res) {
-		data.title = 'Login';
+User.prototype.ViewLogin =  function(req, res) {
+	data.title  = 'Login';	
+	var session = req.session.passport;
+	if (session != undefined ) {
+		var session = req.session.passport.user;
+		if (session === undefined) {
+			res.render('../views/html/login.html',data);
+		} else {
+			res.redirect('/');
+		}
+	} else {
 		res.render('../views/html/login.html',data);
 	}
-	
-
-//register
-User.prototype.register =  function(req, res) {
-	var username  	= req.body.username;
-	var password  	= req.body.password;
 }
 
-User.prototype.viewRegistration =  function(req, res) {
-		data.title = 'Registration Form';
-		res.render('../views/html/registration.html', data);
-	
-	}
-
+User.prototype.ViewRegistration =  function(req, res) {
+	data.title = 'Registration Form';
+	res.render('../views/html/registration.html', data);
+}
 
 // login
-User.prototype.failed =  function(req, res) {
+User.prototype.Failed =  function(req, res) {
 	res.send('failed to login/register');
 }
 
-User.prototype.login = function(req, res){
-	var session = req.session.passport;
-	var title   = 'Welcome Visitor';
-	var content = 'This is the homepage';
-	var user_v 	= 'Visitor';
-	var choice	= 'Login';
-	var link	= '/login';
+User.prototype.Login = function(req, res){
+	var session   	= req.session.passport;
+	var title     	= 'Welcome';
+	var key 	  	= {displayName : 'Visitor'};
+	var unverified	= {title : title, user : key, choice : 'Login', link : '/Login' };
 	if (session === undefined) {
-		res.render('../views/html/profile.html', {title : title , user : user_v, choice : choice, content : content, link : link });
+			res.render('../views/html/profile.html', unverified);
 		} else {
-		var user  = req.session.passport.user;
-		if (user === undefined) {
-			res.render('../views/html/profile.html', {title : title , user : user_v , choice : choice, content : content, link : link});
-		} else {
-			res.render('../views/html/profile.html', {title :'Welcome', user : user, choice : 'Logout', content : content, link : '/logout'});
-			console.log("review data:" , user);
-		}
-		
-	} 
-
+			var user  = req.session.passport.user;
+			if (user === undefined) {
+				res.render('../views/html/profile.html', unverified);
+			} else {
+				// social media login
+				var dName = user.displayName;
+				if (dName != undefined) {
+					res.render('../views/html/profile.html', {title : title, user : user, choice : 'Logout', link : '/Logout'});
+				} else {
+					//local login
+					user = {displayName : user.firstname_sn + ' ' + user.lastname_sn}; //overide displayName
+					res.render('../views/html/profile.html', {title : title, user : user, choice : 'Logout', link : '/Logout'});
+				}
+				// console.log("review data:" , user);
+			}
+		} 
 }
-
 // logout
-User.prototype.logout =  function(req, res) {
+User.prototype.Logout =  function(req, res) {
 	console.log('logging out..');
 	req.logout();
 	res.redirect('/home');
 }
-
 
 module.exports = new User();
