e6f9ebbbfb18a7b9464f4af6f4d1dbf5f083a5ca
jenelynababan <jenelynababan@priume.com>
2016-07-21 14:23:50 +0800

    model

--- models/social_model.js
+++ models/social_model.js
@@ -2,21 +2,21 @@ var host = "http://localhost:8080/";
 var ids  = {
 
   facebook: {
-	  clientID       : '1026341530748830',
-	  clientSecret   : '1e1e2d341568528045a6dd74d66f7d15',
+    clientID       : '144818812612321',
+    clientSecret   : '3e4828da71ce3ec45c6d3f0dc02264ec',
 	  callbackURL    : host+'auth/facebook/callback',
+    profileFields  : ['id', 'displayName', 'photos', 'emails', 'name', 'birthday']
   },
   github: {
-      clientID     : '4feef71d61ffe4fa4f0a',
-      clientSecret : 'da0f99b70ca0ebbb1571bc80a5fcfeb74055a347',
-      callbackURL  : host+"auth/github/callback"
+    clientID     : '4feef71d61ffe4fa4f0a',
+    clientSecret : 'da0f99b70ca0ebbb1571bc80a5fcfeb74055a347',
+    callbackURL  : host+"auth/github/callback"
     },
   instagram: {
     clientID       : '38ea71c7b5374b3fbe1b6bed00f7f403',
     clientSecret   : 'c66af764456b46058d9004f3c42383f4',
     callbackURL    : host+"auth/instagram/callback"
-  }
-  
+  }  
 };
 
 module.exports = ids;
