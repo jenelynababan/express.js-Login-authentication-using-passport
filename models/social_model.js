4667e3b329dc254e9cb553e79362cf948a6b355c
jenelynababan <jenelynababan@priume.com>
2016-07-27 14:23:31 +0800

    query

--- models/social_model.js
+++ models/social_model.js
@@ -1,22 +1,18 @@
-var host = "http://localhost:8080/";
+var host = "http://localhost:8080";
 var ids  = {
 
   facebook: {
-    clientID       : '144818812612321',
-    clientSecret   : '3e4828da71ce3ec45c6d3f0dc02264ec',
-	  callbackURL    : host+'auth/facebook/callback',
-    profileFields  : ['id', 'displayName', 'photos', 'emails', 'name', 'birthday']
+    clientID      : '144818812612321',
+    clientSecret  : '3e4828da71ce3ec45c6d3f0dc02264ec',
+	  callbackURL   : host+'/auth/facebook/callback',
+    profileFields : ['id', 'displayName', 'photos', 'email', 'name', 'age_range','birthday']
   },
-  github: {
-    clientID     : '4feef71d61ffe4fa4f0a',
-    clientSecret : 'da0f99b70ca0ebbb1571bc80a5fcfeb74055a347',
-    callbackURL  : host+"auth/github/callback"
-    },
-  instagram: {
-    clientID       : '38ea71c7b5374b3fbe1b6bed00f7f403',
-    clientSecret   : 'c66af764456b46058d9004f3c42383f4',
-    callbackURL    : host+"auth/instagram/callback"
-  }  
+  google: {
+    clientID     : '90760352671-mb7uhgj7l0fljaoo0tsq3bvb1713d6p4.apps.googleusercontent.com',
+    clientSecret : 'ZYvZzj-V9QQzxGXgCazD6NzT',
+    callbackURL  : host+"/auth/google/callback",
+
+  }   
 };
 
 module.exports = ids;
--- models/user_model.js
+++ models/user_model.js
@@ -6,16 +6,32 @@ UserModel.prototype = model;
 
 // insert
 UserModel.prototype.register = function(data,callback) {
-
-	sql = "INSERT INTO user(emailaddress_sn, password_sn) VALUES (" + mysql.escape(data.username) 
-		+ "," + mysql.escape(data.password) + ")";	
+	sql = "INSERT INTO user(emailaddress_sn, password_sn, firstname_sn, lastname_sn) VALUES (" + mysql.escape(data.username) 
+		+ "," + mysql.escape(data.password) + "," + mysql.escape(data.firstname) + "," + mysql.escape(data.lastname) + ")";	
 	this.execute(sql,callback);
 };
 //check username
 UserModel.prototype.authenticate = function(username, callback) {
-	sql = "SELECT * FROM user where emailaddress_sn  = '"+username+"'";
+	sql = "SELECT * FROM user where emailaddress_sn  = '" + username + "'";
 	this.execute(sql, callback);
-	console.log(sql);
 	};
+// update code for reseting password	
+UserModel.prototype.getCode = function(genCode, username, callback) {
+	sql = "UPDATE user SET generatedCode_sn = " + genCode + " WHERE emailaddress_sn = " + "'" +username + "'" ;	
+	this.execute(sql, callback);
+		// console.log(sql);
+};
+// verify code if match (reset password)
+UserModel.prototype.verifyCode = function(code, callback) {
+	sql = "SELECT * FROM user where generatedCode_sn  = '" + code +"'";
+	this.execute(sql, callback);
+	};
+// Creating new password	
+UserModel.prototype.resetPassword = function(password, username, callback) {
+	sql = "UPDATE user SET password_sn =" + "'" + password + "'" + " WHERE emailaddress_sn = " + "'" + username + "'";
+	this.execute(sql, callback);
+	// console.log(sql);
+};
+
 //exports ContactModel
 module.exports = new UserModel();
