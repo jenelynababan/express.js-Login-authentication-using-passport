31020aaf3bf36cfa42cf77aa0909d37044c87ff9
jenelynababan <jenelynababan@priume.com>
2016-07-27 14:24:24 +0800

    routes

--- config/routes.js
+++ config/routes.js
@@ -5,39 +5,35 @@ module.exports = function(app) {
 	
 	app.get('/', usercontroller.Login);
 	app.get('/Login', usercontroller.ViewLogin);
-	app.get('/Create-account', usercontroller.ViewRegistration);
+	// app.get('/Create-account', usercontroller.ViewRegistration);
 	app.get('/Home', usercontroller.Login);
 	app.get('/Failed', usercontroller.Failed);
 	app.get('/Logout', usercontroller.Logout);
-	// working on passport
-	app.post('/Register', passport.authenticate('register', {
-		failureRedirect: '/Failed',
-		successRedirect: '/Login'
-	}));
+	app.post('/Check-email', usercontroller.CheckEmail);
+	app.post('/Reset-password', usercontroller.ResetPassword);
+	// nodemailer
+	app.post('/Send-code', usercontroller.SendCode);
+	// local auth with passport
 	app.post('/Login', passport.authenticate('login', {
 		failureRedirect: '/Failed',
 		successRedirect: '/Home'
 	}));
-
-	// Socialmedia login with passport
-	app.get('/auth/Facebook',
-		passport.authenticate('facebook'), function(req, res){});
-	app.get('/auth/Facebook/callback', passport.authenticate('facebook', 
+	// app.post('/Register', passport.authenticate('register', {
+	// 	failureRedirect: '/Failed',
+	// 	successRedirect: '/Create-account'
+	// }));
+	// Socialmedia auth with passport
+	app.get('/auth/facebook',
+		passport.authenticate('facebook', { scope: ['email', 'public_profile', 'user_birthday'] }), function(req, res){});
+	app.get('/auth/facebook/callback', passport.authenticate('facebook', 
 		{ failureRedirect: '/Login' }), function(req, res) {
 		res.redirect('/Home');
 	});
 
-	app.get('/auth/github',
-	  passport.authenticate('github'), function(req, res){});
-	app.get('/auth/github/callback', passport.authenticate('github', 
-		{ failureRedirect: '/Login' }), function(req, res) {
-	    res.redirect('/Home');
-	  });
-
-	app.get('/auth/instagram',
-	  passport.authenticate('instagram'), function(req, res){});
-	app.get('/auth/instagram/callback', passport.authenticate('instagram', 
-	  { failureRedirect: '/Login' }), function(req, res) {
+	app.get('/auth/google',
+	  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read'] }), function(req, res){});
+	app.get('/auth/google/callback', passport.authenticate('google', 
+	  { failureRedirect: '/Failed' }), function(req, res) {
 	    res.redirect('/Home');
 	  });
 
