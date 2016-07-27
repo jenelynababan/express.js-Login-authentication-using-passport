f2dd79507254336aea3a3b3e34238ca66b9038b1
jenelynababan <jenelynababan@priume.com>
2016-07-27 16:48:00 +0800

    model

--- models/social_model.js
+++ models/social_model.js
@@ -2,14 +2,14 @@ var host = "http://localhost:8080";
 var ids  = {
 
   facebook: {
-    clientID      : '144818812612321',
-    clientSecret  : '3e4828da71ce3ec45c6d3f0dc02264ec',
+    clientID      : 'get your own',
+    clientSecret  : 'get your own',
 	  callbackURL   : host+'/auth/facebook/callback',
     profileFields : ['id', 'displayName', 'photos', 'email', 'name', 'age_range','birthday']
   },
   google: {
-    clientID     : '90760352671-mb7uhgj7l0fljaoo0tsq3bvb1713d6p4.apps.googleusercontent.com',
-    clientSecret : 'ZYvZzj-V9QQzxGXgCazD6NzT',
+    clientID     : 'get your own',
+    clientSecret : 'get your own',
     callbackURL  : host+"/auth/google/callback",
 
   }   
