c9826496b84078042917fdc062a741c0f345b2bb
jenelynababan <jenelynababan@priume.com>
2016-07-29 11:26:58 +0800

    pulling api

--- models/social_model.js
+++ models/social_model.js
@@ -1,22 +1,18 @@
 var host = "http://localhost:8080";
 var ids  = {
 
-  facebook: {
+  facebook : {
     clientID      : 'get your own',
     clientSecret  : 'get your own',
 	  callbackURL   : host+'/auth/facebook/callback',
     profileFields : ['id', 'displayName', 'photos', 'email', 'name', 'age_range','birthday']
   },
-  google: {
+  google : {
     clientID     : 'get your own',
     clientSecret : 'get your own',
     callbackURL  : host+"/auth/google/callback",
-  }
-  email : {
-    primaryEmail : 'email@mail.com',
-    password     : 'xxxxxxxxxxx'
-  }      
-  
+  },
+ 
 };
 
 module.exports = ids;
